0ef45fef95e041b4eec776cd72457ec71150aa21
Paras <parasv249@gmail.com>
2023-05-02 20:45:47 +0200

    Add search functionality and display results

--- src/component/MovieCard.jsx
+++ src/component/MovieCard.jsx
@@ -2,15 +2,14 @@ import React from 'react'
 
 function MovieCard(props) {
   return (
-    <div className='p-[6px] h-[331px] w-[180px] aspect-video border-2'>
-        <div className="h-[243px] w-full bg-slate-700 border-3 mb-px"><img src="https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg" alt="" srcset="" /></div>
-        <div className="h-[72px] w-full bg-slate-200 border-3">
-            <p className="text-[14px]">Guardians of the Galaxy Vol. 2</p>
+    <div className='drop-shadow-md hover:drop-shadow-xl p-[6px] h-[331px] w-[180px] aspect-video border-2 cursor-pointer hover:scale-105 transition duration-600 flex flex-col justify-between' onClick={props.cardHandle}>
+        <div className="h-3/5 w-full bg-slate-700 border-1 mb-px border-gray-950"><img src={props.poster} alt="Poster Not Found" srcSet="" /></div>
+        <div className="h-1/5 w-full bg-slate-200 border-1 flex flex-col justify-between border-gray-950">
+            <p className="text-[14px]">{props.title}</p>
             <div className="flex justify-between">
-                <p className="text-gray-400 text-[12px]">2017</p>
-                <p className="text-gray-400 text-[12px]">136 min</p>
+                <p className="text-gray-400 text-[12px]">{props.year}</p>
+                <p className="text-gray-400 text-[12px]">{props.type}</p>
             </div>
-            <p className="text-gray-400 text-[12px]">Action, Adventure, Comedy</p>
         </div>
     </div>
   )
