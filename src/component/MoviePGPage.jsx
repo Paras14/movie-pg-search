5092147d8f3749614b2651a4929e1f73847b0145
Paras <parasv249@gmail.com>
2023-06-03 15:12:32 +0200

    Add loading animation

--- src/component/MoviePGPage.jsx
+++ src/component/MoviePGPage.jsx
@@ -1,7 +1,6 @@
 import {React, useEffect, useState} from 'react';
 import { useParams } from 'react-router-dom';
 import SearchBar from './SearchBar';
-import LoadingIcons from 'react-loading-icons';
 
 function MoviePGPage() {
   const [movieData, setMovieData] = useState(null);
@@ -33,6 +32,7 @@ function MoviePGPage() {
         <div className="flex-none">{movieInfo.Title}</div>
         <div className="flex-auto text-sm">({movieInfo.Year})</div>
       </div>
+      
       {movieData?
       <div className='m-2 p-5'>
         <p className="font-normal text-lg"><span className='font-bold'>Advisory Alcohol:</span> {movieData["data"]["advisoryAlcohol"] === 'Be the first to evaluate this category'?'Sorry, but there is no information available on this category right now':movieData["data"]["advisoryAlcohol"]}</p>
@@ -41,8 +41,7 @@ function MoviePGPage() {
         <p className="font-normal text-lg"><span className='font-bold'>Advisory Profanity:</span> {movieData["data"]["advisoryProfanity"] === 'Be the first to evaluate this category'?'Sorry, but there is no information available on this category right now':movieData["data"]["advisoryProfanity"]}</p>
         <p className="font-normal text-lg"><span className='font-bold'>Advisory Violence:</span> {movieData["data"]["advisoryViolence"] === 'Be the first to evaluate this category'?'Sorry, but there is no information available on this category right now':movieData["data"]["advisoryViolence"]}</p>
       </div>
-      : <div className='flex justify-center'>Loading...</div>}
-      {/* <LoadingIcons.Rings /> I have no idea why this is not working, here is the npm page link : https://www.npmjs.com/package/react-loading-icons*/}
+      : <div className='flex flex-col items-center mt-5 gap-2'><div className='text-xl font-semibold'>Loading...</div><svg class="icon-loading" height="90" width="90" viewBox="0 0 90 90"><g fill="#526D82" fill-rule="evenodd" transform="translate(1 1)" stroke="#fff" stroke-width="2"><circle cx="45" cy="45" r="12"><animate attributeName="r" begin="1.5s" dur="3s" values="12;45" calcMode="linear" repeatCount="indefinite"></animate><animate attributeName="stroke-opacity" begin="1.5s" dur="3s" values="1;0" calcMode="linear" repeatCount="indefinite"></animate><animate attributeName="stroke-width" begin="1.5s" dur="3s" values="4;0" calcMode="linear" repeatCount="indefinite"></animate></circle><circle cx="45" cy="45" r="12"><animate attributeName="r" begin="3s" dur="3s" values="12;45" calcMode="linear" repeatCount="indefinite"></animate><animate attributeName="stroke-opacity" begin="3s" dur="3s" values="1;0" calcMode="linear" repeatCount="indefinite"></animate><animate attributeName="stroke-width" begin="3s" dur="3s" values="8;0" calcMode="linear" repeatCount="indefinite"></animate></circle><circle cx="45" cy="45" r="16"><animate attributeName="r" begin="0s" dur="1.5s" values="6;1;2;3;4;5;6" calcMode="linear" repeatCount="indefinite"></animate></circle></g></svg></div>}
     </div>
   )
 }
