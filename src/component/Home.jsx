31e1cea7b33ac7843d49350a67779dc858f425a3
Paras <parasv249@gmail.com>
2023-06-07 16:53:58 +0200

    Fix for different display

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -10,8 +10,8 @@ function Home() {
         <div className="lg:h-1/2 md:h-1/2 sm:h-1/2 h-1/6 flex items-center justify-center">
           <UpArrow className='lg:h-48 lg:w-48 md:h-36 md:w-36 sm:h-36 sm:w-36 w-36 h-36'/>
         </div>
-        <div className='flex items-center justify-center h-1/2'>
-          <div className="text-5xl text-gray-400 p-10 font-pacifico">Search from here<SmilingFace className='w-16 h-16 inline'/></div>
+        <div className='flex items-center justify-center lg:h-1/2 md:h-1/2 sm:h-1/2 h-2/6'>
+          <div className="text-5xl text-gray-400 lg:p-10 md:p-10 sm:p-10 py-10 px-16 font-pacifico">Search from here<SmilingFace className='w-16 h-16 inline'/></div>
         </div>
     </div>
   )
--- src/component/SearchBar.jsx
+++ src/component/SearchBar.jsx
@@ -16,7 +16,7 @@ function SearchBar() {
             </div>
             <div className="flex-intial mx-4 my-4">
             <div class="mb-3">
-                <form class="relative mb-4 flex w-1/2 flex-wrap mx-auto" onSubmit={searchHandle}>
+                <form class="relative mb-4 flex lg:w-1/2 md:w-1/2 sm:w-1/2 w-5/6 flex-wrap mx-auto" onSubmit={searchHandle}>
                 <input
                     type="search"
                     class="relative m-0 block w-[1px] min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
